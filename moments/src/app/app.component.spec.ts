a5969083a9257260a3b2984c903f3f39076a5cbd
Caíque Rodrigues <caique.almeida@extreme.digital>
2024-02-22 13:31:08 -0300

    adc e estilizado o form

--- moments/src/app/app.component.spec.ts
+++ moments/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'moments' title`, () => {
+  it(`should have as title 'Moments'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('moments');
+    expect(app.title).toEqual('Moments');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, moments');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Moments');
   });
 });
