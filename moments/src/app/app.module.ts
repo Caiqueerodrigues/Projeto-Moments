cc82781f987520a02f30d6f807396c8a435113ec
Caíque Rodrigues <caique.almeida@extreme.digital>
2024-02-22 13:27:50 -0300

    adc os modulos controladores de form

--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; //requisições
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; //para formularios
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +20,15 @@ import { HomeComponent } from './components/pages/home/home.component';
     FooterComponent,
     AboutComponent,
     HomeComponent,
+    NewMomentComponent,
+    MomentFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [
     provideClientHydration()
