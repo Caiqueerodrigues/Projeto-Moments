abe234b70459a9915df27cad9c51571a963469b1
Caíque Rodrigues <caique.almeida@extreme.digital>
2024-02-23 10:52:09 -0300

    adc o pag individual

---
+++ moments/src/app/components/pages/moment/moment.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { MomentService } from '../../../services/moment/moment.service';
+import { Moment } from '../../../interfaces/Moment';
+
+
+@Component({
+  selector: 'app-moment',
+  templateUrl: './moment.component.html',
+  styleUrl: './moment.component.css'
+})
+export class MomentComponent {
+  moment?: Moment;
+
+  constructor(private momentService: MomentService, private route: ActivatedRoute) { };
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get("id"));
+
+    this.momentService
+      .getMoment(id)
+      .subscribe(item => this.moment = item.data);
+  }
+
+}
